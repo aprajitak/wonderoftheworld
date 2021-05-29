37e29f5a4b9550364e694387258e32ea7c8f45f0
aprajitak <72867048+aprajitak@users.noreply.github.com>
2021-05-29 21:17:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function one() { 
+  window.alert("The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. ");
+}
+
+function two() {
+  window.alert("The Colosseum is famous because it is an origin of gladiator battles that took place in the time of Roman Empire. ... ");
+}
+
+function three() {
+  window.alert ("Chichen Itza[nb 1] was a large pre-Columbian city built by the Maya people of the Terminal Classic period. The archaeological site is located in Tinúm Municipality, Yucatán State, Mexico.")
+}
+function four() {
+  window.alert ("Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley.");
+}
+function five() {
+  window.alert  ("Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio De Janeiro.");
+}
+function six() {
+  window.alert ("Petra  is a wonder and archeology site.");
+}
+    function seven () {
+  window.alert ("The Great Wall of China is a monument in china.");
+    }
